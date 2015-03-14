b7a1ca8d772e97f246b1414c1385f6b9e12412fc
Oliver <github@oliver3.nl>
2015-03-14 13:49:25 +0100

    Implement positioning elements, fix slide ratio between 4:3 and 17:9, minor fixes

--- ngPresenter.js
+++ ngPresenter.js
@@ -115,10 +115,10 @@
 
         html = html.replace(/^(\s*)\* (.*)$/gm, '$1<li>$2</li>');
 
-        html = html.replace(/^(\s*)$/gm, '$1<hr style="border-width: 0">');
+        //html = html.replace(/^(\s*)$/gm, '$1<hr style="border-width: 0">');
 
         var compiled = $compile(html);
-        content.replaceWith(compiled($scope));
+        content.empty().append(compiled($scope));
 
         $log.log('Registered slide ' + $scope.slideNumber);
 
@@ -148,7 +148,58 @@
       var elementStyle = $element[0].style;
       elementStyle['background-image'] = 'url(' + $attr.image + ')';
       elementStyle['background-repeat'] = 'none';
+      elementStyle['background-size'] = 'cover';
     };
   }]);
+
+  ngPresenter.directive('pos', [function () {
+    return function ($scope, $element, $attr) {
+      var xywh = parseNumberCSV($attr.pos);
+      var x = xywh[0];
+      var y = xywh[1];
+      var w = xywh[2];
+      var h = xywh[3];
+
+      var elementStyle = $element[0].style;
+      if (x !== undefined || y !== undefined) {
+        elementStyle['position'] = 'absolute';
+
+        if (x !== undefined) {
+          if (x >= 0) {
+            elementStyle['left'] = x + '%';
+          } else {
+            elementStyle['right'] = (-x) + '%';
+          }
+        }
+
+        if (y !== undefined) {
+          if (y >= 0) {
+            elementStyle['top'] = y + '%';
+          } else {
+            elementStyle['bottom'] = (-y) + '%';
+          }
+        }
+      }
+      if (w !== undefined) {
+        elementStyle['width'] = w + '%';
+      }
+      if (h !== undefined) {
+        elementStyle['height'] = h + '%';
+      }
+    }
+  }]);
+
+  function parseNumberCSV(csv) {
+    var result = [];
+    if (csv) {
+      var tuple = csv.split(',');
+      for (var i=0; i<tuple.length; i++) {
+        if (tuple[i] && isFinite(tuple[i])) {
+          result[i] = Number(tuple[i]);
+        }
+      }
+    }
+    return result;
+  }
 
 })();
